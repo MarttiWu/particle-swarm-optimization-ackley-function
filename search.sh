66398901d31a5ca33e620f47f42700d19e5c5c27
MarttiWu <69631800+MarttiWu@users.noreply.github.com>
2020-08-31 15:38:41 +0800

    Update search.sh

--- search.sh
+++ search.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 set -x
-#./main [algo] [run] [iter] [filename] [population] [W] [alpha1] [alpha2] [dimension]
+#./main [algo] [run] [iter] [filename] [population] [W] [alpha1] [alpha2] [dimensions]
 ./main pso 30 100 "" 20 0.2 1.49445 1.49445 2
