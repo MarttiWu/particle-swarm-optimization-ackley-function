d2e191d8324d4698ef0a41986caa1860877acbf3
MarttiWu <69631800+MarttiWu@users.noreply.github.com>
2020-08-31 10:36:49 +0800

    Update execute.cpp

--- execute.cpp
+++ execute.cpp
@@ -11,32 +11,19 @@
 void execute(const int Algo,const int runs,const int iterations,const string filename,const int population,const double w,const double a1, const double a2,const int dim){
     srand((unsigned)time(NULL));
     vector<double> avg(iterations/block);
-    //float avg=0;
     if (filename==""){
         for (int i=0;i<runs;i++){
             cout<<"----------------------------------------"<<endl;
             cout<<"run: "<<i<<endl;
             time_t start=0, end=0;
             if (Algo==1){
-                cout<<"exe w: "<<w<<endl;
-                cout<<"exe a1: "<<a1<<endl;
-                cout<<"exe a2: "<<a2<<endl;
-                cout<<"exe dim: "<<dim<<endl;
                 PSO *r = new PSO(iterations, population, w, a1, a2, dim);
                 start = time(NULL);
                 r->Ackley();
-                //r->get_result();
                 for (int j=0;j<iterations/block;j++){
                     avg[j] += r->get_result_value(j*block);
                 }
                 cout<<"Best: "<<r->get_result_value(iterations-1)<<endl;
-                
-                //avg+=r->get_best();
-                //cout<<"best: "<<r->get_best()<<endl;
-                //cout<<"best route: ";
-                //r->print_bestroute();
-                //cout<<"to file"<<endl;
-                //r->to_file();
                 cout<<endl;
                 end = time(NULL);
             }
